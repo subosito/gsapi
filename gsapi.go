cbc8534b057baad813e3f09cc18c14293941a14f
Alif Rachmawadi <subosito@gmail.com>
2014-08-29 22:07:48 +0700

    refactor client

--- gsapi.go
+++ gsapi.go
@@ -23,23 +23,35 @@ func NewClient(httpClient *http.Client) *Client {
 	return &Client{url: u, client: httpClient}
 }
 
+func (c *Client) GetRequest(v url.Values) (*http.Request, error) {
+	u := c.url
+	u.RawQuery = v.Encode()
+	return http.NewRequest("GET", u.String(), nil)
+}
+
+func (c *Client) Do(r *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if v != nil {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	}
+
+	return resp, err
+}
+
 func (c *Client) Package(pkg string) (Package, error) {
 	p := Package{}
 	d := url.Values{}
 	d.Set("action", "package")
 	d.Set("id", pkg)
 
-	u := c.url
-	u.RawQuery = d.Encode()
-
-	resp, err := c.client.Get(u.String())
-	if err != nil {
-		return p, err
-	}
-
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&p)
+	req, _ := c.GetRequest(d)
+	_, err := c.Do(req, &p)
 	if err != nil {
 		return p, err
 	}
