7011272005fd131c52763e5d40d517a0c5a89146
Alif Rachmawadi <subosito@gmail.com>
2014-08-31 00:26:07 +0700

    added documentation

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # gsapi
 
 [![Build Status](https://travis-ci.org/subosito/gsapi.svg?branch=master)](https://travis-ci.org/subosito/gsapi)
+[![GoDoc](https://godoc.org/github.com/subosito/gsapi?status.png)](https://godoc.org/github.com/subosito/gsapi)
 
--- gsapi.go
+++ gsapi.go
@@ -1,3 +1,4 @@
+// Package gsapi provides simple wrapper for Go Search API (http://go-search.org/)
 package gsapi
 
 import (
@@ -6,13 +7,16 @@ import (
 	"net/url"
 )
 
+// BaseURL is the base endpoint for Go Search API
 const BaseURL = "http://go-search.org/api"
 
+// Client manages communication with Go Search API
 type Client struct {
 	url    *url.URL
 	client *http.Client
 }
 
+// NewClient returns a new Go Search Client. This will load http.DefaultClient if httpClient is nil.
 func NewClient(httpClient *http.Client) *Client {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
@@ -23,12 +27,14 @@ func NewClient(httpClient *http.Client) *Client {
 	return &Client{url: u, client: httpClient}
 }
 
+// GetRequest constructs http.Request which points to Go Search API URL.
 func (c *Client) GetRequest(v url.Values) (*http.Request, error) {
 	u := c.url
 	u.RawQuery = v.Encode()
 	return http.NewRequest("GET", u.String(), nil)
 }
 
+// Do performs actual request and decode json output as proper struct object.
 func (c *Client) Do(r *http.Request, v interface{}) (*http.Response, error) {
 	resp, err := c.client.Do(r)
 	if err != nil {
@@ -50,6 +56,7 @@ func (c *Client) perform(uv url.Values, v interface{}) error {
 	return err
 }
 
+// Package returns information for particular package.
 func (c *Client) Package(pkg string) (Package, error) {
 	p := Package{}
 	d := url.Values{}
@@ -64,6 +71,7 @@ func (c *Client) Package(pkg string) (Package, error) {
 	return p, nil
 }
 
+// Tops returns best packages based on categories.
 func (c *Client) Tops() ([]Top, error) {
 	t := []Top{}
 	d := url.Values{}
@@ -78,6 +86,7 @@ func (c *Client) Tops() ([]Top, error) {
 	return t, nil
 }
 
+// Packages returns ID of all registered packages.
 func (c *Client) Packages() (Packages, error) {
 	s := Packages{}
 	d := url.Values{}
@@ -91,6 +100,7 @@ func (c *Client) Packages() (Packages, error) {
 	return s, nil
 }
 
+// Search returns Result based on q search results.
 func (c *Client) Search(q string) (Result, error) {
 	r := Result{}
 	d := url.Values{}
